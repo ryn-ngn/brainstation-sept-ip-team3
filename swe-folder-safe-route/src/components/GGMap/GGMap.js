d1bd32979db24a384a18c8f75885e1e176ff07c3
Ryan Nguyen <ryan.ngn@outlook.com>
2024-04-20 14:52:43 -0400

    swe: reorganize structure

--- swe-folder-safe-route/src/components/GGMap/GGMap.js
+++ swe-folder-safe-route/src/components/GGMap/GGMap.js
@@ -1,3 +1,63 @@
+import "./GGMap.scss";
+import { useState } from "react";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { useJsApiLoader, Autocomplete } from "@react-google-maps/api";
+import Header from "../Header/Header";
+import MapDetail from "../MapDetail/MapDetail";
+import Routes from "../Routes";
+const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+
 export default function GGMap() {
-  return <div>This is Google Map area</div>;
+  const center = { lat: 43.64238, lng: -79.39674 };
+  const [origin, setOrigin] = useState("");
+  const [destination, setDestination] = useState("");
+  const [routes, setRoutes] = useState([]);
+  const [routeIndex, setRouteIndex] = useState(0);
+  const [mapCenter, setMapCenter] = useState(true);
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: apiKey,
+    libraries: ["places"],
+  });
+
+  if (!isLoaded) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    <div className="maps">
+      <Header
+        setOrigin={setOrigin}
+        setDestination={setDestination}
+        setMapCenter={setMapCenter}
+        Autocomplete={Autocomplete}
+      />
+      <div className="maps__container">
+        <APIProvider apiKey={apiKey}>
+          <Map
+            defaultCenter={center}
+            defaultZoom={15}
+            zoomControl={false}
+            streetViewControl={false}
+            mapTypeControl={false}
+            fullscreenControl={false}
+          >
+            <Routes
+              origin={origin}
+              destination={destination}
+              routeIndex={routeIndex}
+              setRoutes={setRoutes}
+              mapCenter={mapCenter}
+            />
+          </Map>
+        </APIProvider>
+      </div>
+      <MapDetail
+        origin={origin}
+        destination={destination}
+        routes={routes}
+        setRouteIndex={setRouteIndex}
+      />
+    </div>
+  );
 }
--- swe-folder-safe-route/src/components/Routes.js
+++ swe-folder-safe-route/src/components/Routes.js
@@ -2,38 +2,39 @@ import { useState, useEffect } from "react";
 import { useMapsLibrary, useMap } from "@vis.gl/react-google-maps";
 
 function Routes({ origin, destination, routeIndex, setRoutes, mapCenter }) {
-	const map = useMap();
-	const routesLibrary = useMapsLibrary("routes");
-	const [directionsService, setDirectionsService] = useState();
-	const [directionsRenderer, setDirectionsRenderer] = useState();
+  const map = useMap();
+  const routesLibrary = useMapsLibrary("routes");
+  const [directionsService, setDirectionsService] = useState();
+  const [directionsRenderer, setDirectionsRenderer] = useState();
 
-	useEffect(() => {
-		if (!map || !routesLibrary) return;
-		setDirectionsService(new routesLibrary.DirectionsService());
-		setDirectionsRenderer(new routesLibrary.DirectionsRenderer({ map }));
-	}, [routesLibrary, map]);
+  useEffect(() => {
+    if (!map || !routesLibrary) return;
+    setDirectionsService(new routesLibrary.DirectionsService());
+    setDirectionsRenderer(new routesLibrary.DirectionsRenderer({ map }));
+  }, [routesLibrary, map]);
 
-	useEffect(() => {
-		const getRoutes = async () => {
-			if (!directionsService || !directionsRenderer || !origin || !destination) return;
-			const response = await directionsService.route({
-				origin: origin,
-				destination: destination,
-				travelMode: window.google.maps.TravelMode.WALKING,
-				provideRouteAlternatives: true,
-			});
-			directionsRenderer.setDirections(response);
-			setRoutes(response.routes);
-		};
-		getRoutes();
-	}, [directionsService, directionsRenderer, origin, destination, mapCenter]);
+  useEffect(() => {
+    const getRoutes = async () => {
+      if (!directionsService || !directionsRenderer || !origin || !destination)
+        return;
+      const response = await directionsService.route({
+        origin: origin,
+        destination: destination,
+        travelMode: window.google.maps.TravelMode.WALKING,
+        provideRouteAlternatives: true,
+      });
+      directionsRenderer.setDirections(response);
+      setRoutes(response.routes);
+    };
+    getRoutes();
+  }, [directionsService, directionsRenderer, origin, destination, mapCenter]);
 
-	useEffect(() => {
-		if (!directionsRenderer) return;
-		directionsRenderer.setRouteIndex(routeIndex);
-	}, [routeIndex, directionsRenderer]);
+  useEffect(() => {
+    if (!directionsRenderer) return;
+    directionsRenderer.setRouteIndex(routeIndex);
+  }, [routeIndex, directionsRenderer]);
 
-	return <div>Routes</div>;
+  return <div>Routes</div>;
 }
 
 export default Routes;
