de8a282cdb374899660a572ef799abb596a14886
Tonie Opara <146739904+tonynuns@users.noreply.github.com>
2024-04-22 20:00:48 -0600

    Link to home page

--- swe-folder-safe-route/src/pages/Arrived/Arrived.js
+++ swe-folder-safe-route/src/pages/Arrived/Arrived.js
@@ -1,34 +1,37 @@
 import "./Arrived.scss";
 import NotificationModal from "../../components/NotificationModal/NotificationModal";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import rating from "../../assets/images/ratingScrShot.png";
 
 export default function Arrived() {
-  const [showRating, setShowRating] = useState(false);
+	const [showRating, setShowRating] = useState(false);
 
-  const handleCloseModal = (e) => {
-    setShowRating(false);
-  };
-  return (
-    <div className="arrived">
-      {/* <img src={onRoute} alt="google map on-route"></img> */}
-      <NotificationModal
-        heading={
-          "Would you like to let your selected contacts know that you've made it to your destination?"
-        }
-        content={"Only your selected contacts will get the notification."}
-        arrivalNoticeRequired={true}
-        setShowRating={setShowRating}
-      />
+	const handleCloseModal = (e) => {
+		setShowRating(false);
+	};
+	return (
+		<div className="arrived">
+			{/* <img src={onRoute} alt="google map on-route"></img> */}
+			<NotificationModal
+				heading={
+					"Would you like to let your selected contacts know that you've made it to your destination?"
+				}
+				content={"Only your selected contacts will get the notification."}
+				arrivalNoticeRequired={true}
+				setShowRating={setShowRating}
+			/>
 
-      {showRating && (
-        <div className="modal-overlay">
-          <div onClick={handleCloseModal} className="modal-content">
-            <h2>Notification Sent</h2>
-            <img src={rating} alt="rating modal"></img>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+			{showRating && (
+				<div className="modal-overlay">
+					<Link to="/">
+						<div onClick={handleCloseModal} className="modal-content">
+							<h2>Notification Sent</h2>
+							<img src={rating} alt="rating modal"></img>
+						</div>
+					</Link>
+				</div>
+			)}
+		</div>
+	);
 }
