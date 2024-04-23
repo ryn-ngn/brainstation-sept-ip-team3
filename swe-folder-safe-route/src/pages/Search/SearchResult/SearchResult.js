6ae16a81b1bfff11a78be373f96ae8eac9cc895c
Tonie Opara <146739904+tonynuns@users.noreply.github.com>
2024-04-22 19:41:39 -0600

    route link

--- swe-folder-safe-route/src/pages/Search/SearchResult/SearchResult.js
+++ swe-folder-safe-route/src/pages/Search/SearchResult/SearchResult.js
@@ -3,20 +3,17 @@ import "./SearchResult.scss";
 import { useNavigate } from "react-router-dom";
 
 export default function SearchResult() {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const handleSearchClick = () => {
-    navigate("/routes");
-  };
+	const handleSearchClick = () => {
+		navigate("/routes");
+	};
 
-  return (
-    <div className="search-result">
-      <img src={searchResultSrcShot} alt="gg map search page"></img>
+	return (
+		<div className="search-result">
+			<img onClick={handleSearchClick} src={searchResultSrcShot} alt="gg map search page"></img>
 
-      <div
-        onClick={handleSearchClick}
-        className="search-result__get-direction"
-      ></div>
-    </div>
-  );
+			<div onClick={handleSearchClick} className="search-result__get-direction"></div>
+		</div>
+	);
 }
